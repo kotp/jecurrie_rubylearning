97ca0861f7dcc12edc180030401184f8e662e84a
Victor Goff <keeperotphones@gmail.com>
2016-03-08 08:06:55 -0500

    Simple script to install three gems

---
+++ install_these_gems.rb
@@ -0,0 +1,6 @@
+puts 'Getting ready to install rubydoctest, rubocop and RLCoreKickstart'
+puts 'Press enter to install these three gems, or control-c to abort'
+gets
+puts `gem install rubydoctest`
+puts `gem install RLCoreKickstart`
+puts `gem install rubocop`
