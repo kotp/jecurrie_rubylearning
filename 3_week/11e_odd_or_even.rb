a7b1eb68779c3d9eec24e279565d14cd6d2df8f3
Victor Goff <keeperotphones@gmail.com>
2016-03-22 11:03:46 -0400

    The odd_or_even method cleans up the running code

--- 3_week/11e_odd_or_even.rb
+++ 3_week/11e_odd_or_even.rb
@@ -1,12 +1,8 @@
+def odd_or_even(number)
+  number.odd? ? 'odd' : 'even'
+end
 collection = [12, 23, 456, 123, 4579]
 
 collection.each do |c|
-  print "The number #{c} and it is "
-  # puts collection % 2
-  answer = if c.even?
-             'even.'
-           else
-             'odd.'
-           end
-  puts answer
+  puts "The number #{c} and it is #{odd_or_even(c)}."
 end
