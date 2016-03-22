09f16b7fe98fbf4429007f054d897e9aade70f3b
JoanCurrie <jecurrie8@gmail.com>
2016-03-22 08:42:13 -0400

    Update 11e_odd_or_even.rb

---
+++ 3_week/11e_odd_or_even.rb
@@ -0,0 +1,11 @@
+collection = [12, 23, 456, 123, 4579]
+
+collection.each do |collection|
+	print "The number #{collection} and it is "
+	#puts collection % 2
+	if (collection % 2 == 0)
+		puts "even."
+	else
+	  puts "odd."
+	end  	
+end	
