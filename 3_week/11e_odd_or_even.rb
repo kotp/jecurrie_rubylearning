6a8406f8cffcc7d8637399c61cb40c2dd58004b2
JoanCurrie <jecurrie8@gmail.com>
2016-03-22 15:46:32 -0400

    Update 11e_odd_or_even.rb

--- 3_week/11e_odd_or_even.rb
+++ 3_week/11e_odd_or_even.rb
@@ -4,8 +4,8 @@ collection.each do |collection|
 	print "The number #{collection} and it is "
 	#puts collection % 2
 	if (collection % 2 == 0)
-	  "even."
+	  puts "even."
 	else
-	  "odd."
+	  puts "odd."
 	end  	
 end	
