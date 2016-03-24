f69d2f95ab42aa0442b38cd0f86220f73fab353a
JoanCurrie <jecurrie8@gmail.com>
2016-03-24 17:41:25 -0400

    Update 2e_inserted_word.rb

--- 3_week/2e_inserted_word.rb
+++ 3_week/2e_inserted_word.rb
@@ -4,17 +4,35 @@
 # automatically be closed when the block terminates  
 
 i = 0
-myfile = File.open('insertword_w3.txt', 'r+') do |f1| #|line|  
+puts i
+myfile = File.open("insertword_w3.txt", "r+") do |f1| 
 
 #=begin
-  while c = f1.getc
+  while c = f1.getc.chr
   	#print i
     #puts c.chr
+    #puts c.chr
+    #puts "hello"
+  #IO.read('insertword_w3.txt')
+
     if c.chr == "w"
-   	f1.print "ins"
-    	c.chr = i - 1
+    	#puts "inthecchr"
+   	  f1.write("inserted word text text\n")
+   	  #f1.puts 
+      #puts "inserted word text text"
+      i = 3
+      if i == 3 
+    #	  puts "i is 3"
+    	  break
+      end	
+      break
     end	
-    i = i + 1
+    if i == 3 
+    #  puts "i is 3"
+      break
+    end	
+
   end
 #=end
 end 
+#
