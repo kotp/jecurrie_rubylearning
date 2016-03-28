d6c1fa971b50156e550ec4fe35f1bfcc0842b928
Victor Goff <keeperotphones@gmail.com>
2016-03-28 15:05:39 -0400

    Added the tests back in removed minitest lines
    The RLCoreKicstart gem requires the minitest/autorun automatically.
    The issue with it, maybe on Windows, where "pride" may not work
    correctly, I simply don't know.
    
    The resulting tests though should report "well enough".
    
    This commit produces 3 failures, no errors and 0 skips with 4
    assertions.

--- 3_week/9e_reverse_word_order.rb
+++ 3_week/9e_reverse_word_order.rb
@@ -1,7 +1,3 @@
-
-gem "minitest"
-require "minitest/autorun"
-
 def reverse_word_order(a_string)
   elements = a_string.split("\n")
   elements = a_string.split(" ")
@@ -22,3 +18,16 @@ require 'rlcorekickstart/evaluate_me'
 include EvaluateMe
 eval(EVALUATE)
 __END__
+ZGVzY3JpYmUgJ3JldmVyc2Vfd29yZF9vcmRlcicgZG8KICBpdCAnbXVzdCBy
+ZXZlcnNlIHRoZSB3b3JkIG9yZGVyJyBkbwogICAgc3RyaW5nID0gJ2hlbGxv
+IHdvcmxkJwogICAgcmV2ZXJzZV93b3JkX29yZGVyKHN0cmluZykubXVzdF9l
+cXVhbCAnd29ybGQgaGVsbG8nCiAgZW5kCgogIGl0ICdtdXN0IG5vdCBjaGFu
+Z2UgdGhlIGNhc2UnIGRvCiAgICBzdHJpbmcgPSAnSGVsbG8gV29ybGQnCiAg
+ICByZXZlcnNlX3dvcmRfb3JkZXIoc3RyaW5nKS5tdXN0X2VxdWFsICdXb3Js
+ZCBIZWxsbycKICBlbmQKCiAgaXQgJ211c3Qgbm90IHJldmVyc2UgdGhlIGxl
+dHRlcnMgaW4gdGhlIHdvcmQnIGRvCiAgICBzdHJpbmcgPSAnYWJjZGVmZycK
+ICAgIHJldmVyc2Vfd29yZF9vcmRlcihzdHJpbmcpLndvbnRfZXF1YWwgJ2dm
+ZWRjYmEnCiAgZW5kCgogIGl0ICdtdXN0IG5vdCBjaGFuZ2UgZHJvcCBwdW5j
+dHVhdGlvbicgZG8KICAgIHN0cmluZyA9ICd0aGUuIGVuZC4gb2YuIHRpbWVz
+LicKICAgIHJldmVyc2Vfd29yZF9vcmRlcihzdHJpbmcpLm11c3RfZXF1YWwg
+J3RpbWVzLiBvZi4gZW5kLiB0aGUuJwogIGVuZAplbmQK
