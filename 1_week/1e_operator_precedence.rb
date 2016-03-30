ffa4957fa1b218c25e3adbffd66a2cfd50728451
JoanCurrie <jecurrie8@gmail.com>
2016-03-29 21:49:07 -0400

    Update 1e_operator_precedence.rb

--- 1_week/1e_operator_precedence.rb
+++ 1_week/1e_operator_precedence.rb
@@ -2,6 +2,10 @@ y = false
 z = true
 x = y or z
 #x = (y=false) or (z = true)
+# =  Assignment
+# or Logical composition
+
+#x =  Assignment (y=false)  or Logical composition (z = true)
 puts x 
 (x = y) or z
 puts x 
