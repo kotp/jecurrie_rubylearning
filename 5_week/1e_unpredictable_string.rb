003ad5f3f96a446d3678ecc974706ffe75841bb6
JoanCurrie <jecurrie8@gmail.com>
2016-04-04 15:10:18 -0400

    Update 1e_unpredictable_string.rb

--- 5_week/1e_unpredictable_string.rb
+++ 5_week/1e_unpredictable_string.rb
@@ -1,23 +1,20 @@
 class UnpredictableString < String
-  def initialize (string)
-  	#Instance variable.
-    @a_string = string 
-    @another_string = ""
-  end	
-  def scramble
-    an_array = @a_string.split(//)
+  def scramble(a_string)
+    an_array = a_string.split(//)
     length = an_array.length
+    another_string = ""
     for i in 1..length
-    	an_integer = rand(length) #j)
-      @another_string = @another_string  + an_array[an_integer].to_s
+    	an_integer = rand(length) 
+      another_string = another_string  + an_array[an_integer].to_s
     	an_array.delete_at(an_integer)
     	length = length - 1
     end	
  
-     @another_string    
+     puts another_string    
   end
 
 end
 
-s = UnpredictableString.new("It was a dark and stormy night.")
-s.scramble
+s = UnpredictableString.new 
+s.scramble("It was a dark and stormy night.")
+
